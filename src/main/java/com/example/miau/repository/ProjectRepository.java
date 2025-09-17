d72e4ebf96f0557c44599cff99553d903823ead3
Joseph <135732587+Josefuufuu@users.noreply.github.com>
2025-09-17 18:11:42 -0500

    aaa

--- src/main/java/com/example/miau/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.miau.repository;
-
-import com.example.miau.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
